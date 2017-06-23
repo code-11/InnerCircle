87e113be2429041083d1cbb7e8a2318418bfeee4
code-11 <brendan.ritter@yahoo.com>
2017-06-22 20:17:21 -0400

    Finally got things playing nicely

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
-import * as $ from 'jquery';
-class Startup {
-    static main() {
-        console.log('Hello World');
-        return 0;
+define(["require", "exports", "jquery"], function (require, exports, $) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    class Startup {
+        static main() {
+            console.log('Hello World');
+            return 0;
+        }
     }
-}
-$(document).ready(function () {
-    Startup.main();
+    $(document).ready(function () {
+        Startup.main();
+    });
 });
 //# sourceMappingURL=main.js.map
