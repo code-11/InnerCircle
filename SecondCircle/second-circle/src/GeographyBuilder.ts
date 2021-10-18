e758241761c108f04842fdfd7a55464d20dc66ad
code-11 <brendan.ritter@yahoo.com>
2021-10-18 18:45:56 -0400

    Continued on river generation

--- SecondCircle/second-circle/src/GeographyBuilder.ts
+++ SecondCircle/second-circle/src/GeographyBuilder.ts
@@ -45,10 +45,14 @@ export const floodFill=(x:number, y:number, type:TileType, tileMap:{[key: string
     }
 }
 
+const slope=(pt1 : number[], pt2:number[])=>{
+    return (pt2[1]-pt1[1])/(pt2[0]-pt1[0]);
+}
+
 export const line=(x1:number,y1:number,x2:number,y2:number)=>{
     const lineFunc=(x:number)=>{
-        const slope=(y2-y1)/(x2-x1);
-        return (slope * (x - x1)) +y1;
+        const slopeVal=slope([x1,y1],[x2,y2]);
+        return (slopeVal * (x - x1)) +y1;
     }
     if (x1===x2){
         throw new Error("Invalid arguments. Xs must be different values")
@@ -62,6 +66,14 @@ export const line=(x1:number,y1:number,x2:number,y2:number)=>{
     return toReturn;
 }
 
+export const midpoint=(pt1:number[],pt2:number[])=>{
+    return [(pt2[0]+pt1[0])/2, (pt2[1]+pt1[1])/2];
+}
+
+export const distance=(pt1: number[], pt2:number[])=>{
+    return Math.sqrt(Math.pow(pt2[0]-pt1[0],2) + Math.pow(pt2[1]-pt1[1],2))
+}
+
 export const ptKey=(pt:number[])=>pt[0]+"-"+pt[1];
 
 export class GeographyBuilder{
@@ -73,7 +85,7 @@ export class GeographyBuilder{
         this.height=height;
     }
 
-    createMountain(){
+    createMountain():Tile[]{
         const maxMountainLine=this.height/10;
         const startHeight = getRandomInt(maxMountainLine);
         const endHeight = getRandomInt(maxMountainLine);
@@ -106,7 +118,7 @@ export class GeographyBuilder{
         return allMtPoints.map((pt)=>{return {point: pt,tileType:TileType.Rock}});
     }
 
-    createRiver(){
+    createRiver(mtPoints:Tile[]){
         const possibleEndpoints=[];
         for(let i=this.height/2;i<this.height-1;i+=1){
             possibleEndpoints.push([0,i]);
@@ -118,8 +130,53 @@ export class GeographyBuilder{
             possibleEndpoints.push([this.width-1,i]);
         }
         const endPoint= choose(possibleEndpoints);
+        const startTest=(pt:number[])=>{
+            if (endPoint[0] > this.width/2){
+                return pt[0] <this.width/2; 
+            }else{
+                return pt[0] >=this.width/2;
+            }
+        }
+        let possibleStartPoint=choose(mtPoints).point;
+        while(!startTest(possibleStartPoint)){
+            possibleStartPoint=choose(mtPoints).point;
+        }
         
-
+        //Now we have start and end points. Iterate midpoint drifting.
+        const midpointDrift=(start:number[],end : number[],depth: number=0)=>{
+            if (depth==4){
+                return line(start[0],start[1],end[0],end[1]);
+            }else{
+                const midPt=midpoint(start,end);
+                const slopeVal=slope(start,end);
+                const perpSlopeVal=-1/slopeVal;
+                const origDis=distance(start,end);
+                const newDis= origDis/4;
+                const randSide=choose([1,-1]);
+                const newMidPointX=midPt[0] + (randSide*newDis)/Math.sqrt(1+Math.pow(perpSlopeVal,2)); 
+                const newMidPointY=(perpSlopeVal * (newMidPointX - midPt[0])) +midPt[1];
+                let newMidPoint=[newMidPointX,newMidPointY];
+                // if (newMidPointX >= 0 && newMidPointX<this.width-1 && newMidPointY >= 0 && newMidPointY< this.width-1){
+                //     newMidPoint=[newMidPointX,newMidPointY];
+                // }else{
+                //     const newMidPointX2=midPt[0] - (randSide*newDis)/Math.sqrt(1+Math.pow(perpSlopeVal,2)); 
+                //     const newMidPointY2=(perpSlopeVal * (newMidPointX - midPt[0])) +midPt[1];
+                //     if (newMidPointX2 >= 0 && newMidPointX2<this.width-1 && newMidPointY2 >= 0 && newMidPointY2< this.width-1){
+                //         newMidPoint=[newMidPointX2,newMidPointY2];
+                //     }else{
+                //         return line(start[0],start[1],end[0],end[1]);
+                //     }
+                // }
+                const firstHalf : number[][] = midpointDrift(start, newMidPoint,depth+1);
+                const secondHalf : number[][] = midpointDrift(newMidPoint,end,depth+1);
+                return firstHalf.concat(secondHalf);
+                // return [[Math.round(newMidPointX), Math.round(newMidPointY)],[Math.round(midPt[0]),Math.round(midPt[1])]];
+                // return [Math.round(midPt[0]),Math.round(midPt[1])];
+            }
+        }
+        const toReturn = midpointDrift(possibleStartPoint,endPoint);
+        return toReturn.map((pt)=>[Math.round(pt[0]),Math.round(pt[1])]);
+        // return [possibleStartPoint,drift[0],drift[1],endPoint];
     }
 
     createLocalmap(){
@@ -139,6 +196,11 @@ export class GeographyBuilder{
         }
 
         floodFill(0,0,TileType.Rock,tileMap);
+
+        const riverPts=this.createRiver(mtTiles);
+        riverPts.forEach((pt)=>{
+            tileMap[ptKey(pt)].tileType=TileType.Water;
+        })
 
         return Object.values(tileMap).sort((v1,v2)=>{
             if (v2.point[1]<v1.point[1]){
