9b76a5455cbfdfbbb007da089ef7a5b7b2a30c72
code-11 <brendan.ritter@yahoo.com>
2021-10-24 14:46:00 -0400

    Finished river generation

--- SecondCircle/second-circle/src/GeographyBuilder.ts
+++ SecondCircle/second-circle/src/GeographyBuilder.ts
@@ -49,22 +49,65 @@ const slope=(pt1 : number[], pt2:number[])=>{
     return (pt2[1]-pt1[1])/(pt2[0]-pt1[0]);
 }
 
-export const line=(x1:number,y1:number,x2:number,y2:number)=>{
-    const lineFunc=(x:number)=>{
-        const slopeVal=slope([x1,y1],[x2,y2]);
-        return (slopeVal * (x - x1)) +y1;
+export const line=(x0:number, y0:number, x1:number, y1:number)=>{
+    x0=Math.round(x0);
+    x1=Math.round(x1);
+    y0=Math.round(y0);
+    y1=Math.round(y1);
+    const dx = Math.abs(x1 - x0)    // distance to travel in X
+    const dy = Math.abs(y1 - y0)    // distance to travel in Y
+
+    let ix :number, iy: number;
+    if (x0 < x1){
+        ix = 1           // x will increase at each step
+    }else{
+        ix = -1          // x will decrease at each step
     }
-    if (x1===x2){
-        throw new Error("Invalid arguments. Xs must be different values")
+
+    if (y0 < y1){
+        iy = 1           // y will increase at each step
+    }else{
+        iy = -1          // y will decrease at each step
     }
-    const diff = Math.sign(x2-x1);
-    const toReturn=[[x1,y1]];
-    for(let i=x1+1;i!==x2;i+=diff){
-        toReturn.push([i,Math.round(lineFunc(i))]);
-    }
-    toReturn.push([x2,y2])
+
+    let e = 0;                // Current error 
+    let e1:number,e2:number;
+    const toReturn: number[][] =[];
+    [...Array(dx+dy).keys()].forEach((i)=>{
+        toReturn.push([x0,y0]);
+        e1 = e + dy;
+        e2 = e - dx;
+        if (Math.abs(e1) < Math.abs(e2)){
+            // Error will be smaller moving on X
+            x0 += ix;
+            e = e1;
+        }else{
+            // Error will be smaller moving on Y
+            y0 += iy;
+            e = e2;
+        }
+    });
+    toReturn.push([x1,y1]);
     return toReturn;
+
 }
+
+// export const line=(x1:number,y1:number,x2:number,y2:number)=>{
+//     const lineFunc=(x:number)=>{
+//         const slopeVal=slope([x1,y1],[x2,y2]);
+//         return (slopeVal * (x - x1)) +y1;
+//     }
+//     if (x1===x2){
+//         throw new Error("Invalid arguments. Xs must be different values")
+//     }
+//     const diff = Math.sign(x2-x1);
+//     const toReturn=[[x1,y1]];
+//     for(let i=x1+1;i!==x2;i+=diff){
+//         toReturn.push([i,Math.round(lineFunc(i))]);
+//     }
+//     toReturn.push([x2,y2])
+//     return toReturn;
+// }
 
 export const midpoint=(pt1:number[],pt2:number[])=>{
     return [(pt2[0]+pt1[0])/2, (pt2[1]+pt1[1])/2];
@@ -107,13 +150,13 @@ export class GeographyBuilder{
         rndMtLinePoints.push([this.width-1,endHeight]);
         let allMtPoints : number[][]=[];
         const pairwiseMtPoints=pairwise(rndMtLinePoints);
-        pairwiseMtPoints.forEach((ptPair)=>{
+        pairwiseMtPoints.forEach((ptPair,i)=>{
             const p1=ptPair[0];
             const p2=ptPair[1];
             const linePoints=line(p1[0],p1[1]+5,p2[0],p2[1]+5);
-            const linePointsB=line(p1[0],p1[1]+6,p2[0],p2[1]+6);
-            const linePointsC=line(p1[0],p1[1]+7,p2[0],p2[1]+7);
-            allMtPoints=allMtPoints.concat(linePoints).concat(linePointsB).concat(linePointsC);
+            // const linePointsB=line(p1[0],p1[1]+6,p2[0],p2[1]+6);
+            // const linePointsC=line(p1[0],p1[1]+7,p2[0],p2[1]+7);
+            allMtPoints=allMtPoints.concat(linePoints);//.concat(linePointsB).concat(linePointsC);
         });
         return allMtPoints.map((pt)=>{return {point: pt,tileType:TileType.Rock}});
     }
@@ -156,17 +199,17 @@ export class GeographyBuilder{
                 const newMidPointX=midPt[0] + (randSide*newDis)/Math.sqrt(1+Math.pow(perpSlopeVal,2)); 
                 const newMidPointY=(perpSlopeVal * (newMidPointX - midPt[0])) +midPt[1];
                 let newMidPoint=[newMidPointX,newMidPointY];
-                // if (newMidPointX >= 0 && newMidPointX<this.width-1 && newMidPointY >= 0 && newMidPointY< this.width-1){
-                //     newMidPoint=[newMidPointX,newMidPointY];
-                // }else{
-                //     const newMidPointX2=midPt[0] - (randSide*newDis)/Math.sqrt(1+Math.pow(perpSlopeVal,2)); 
-                //     const newMidPointY2=(perpSlopeVal * (newMidPointX - midPt[0])) +midPt[1];
-                //     if (newMidPointX2 >= 0 && newMidPointX2<this.width-1 && newMidPointY2 >= 0 && newMidPointY2< this.width-1){
-                //         newMidPoint=[newMidPointX2,newMidPointY2];
-                //     }else{
-                //         return line(start[0],start[1],end[0],end[1]);
-                //     }
-                // }
+                if (newMidPointX >= 0 && newMidPointX<this.width-1 && newMidPointY >= 0 && newMidPointY< this.width-1){
+                    newMidPoint=[newMidPointX,newMidPointY];
+                }else{
+                    const newMidPointX2=midPt[0] - (randSide*newDis)/Math.sqrt(1+Math.pow(perpSlopeVal,2)); 
+                    const newMidPointY2=(perpSlopeVal * (newMidPointX - midPt[0])) +midPt[1];
+                    if (newMidPointX2 >= 0 && newMidPointX2<this.width-1 && newMidPointY2 >= 0 && newMidPointY2< this.width-1){
+                        newMidPoint=[newMidPointX2,newMidPointY2];
+                    }else{
+                        return line(start[0],start[1],end[0],end[1]);
+                    }
+                }
                 const firstHalf : number[][] = midpointDrift(start, newMidPoint,depth+1);
                 const secondHalf : number[][] = midpointDrift(newMidPoint,end,depth+1);
                 return firstHalf.concat(secondHalf);
@@ -199,7 +242,12 @@ export class GeographyBuilder{
 
         const riverPts=this.createRiver(mtTiles);
         riverPts.forEach((pt)=>{
+            try{
             tileMap[ptKey(pt)].tileType=TileType.Water;
+            }
+            catch{
+                console.log("herp!");
+            }
         })
 
         return Object.values(tileMap).sort((v1,v2)=>{
